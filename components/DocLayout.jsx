e9424cba2568e95782b481b01cb2f0a6cdfa1d5f
onesine <onesine@dmdconsul.com>
2022-11-18 21:25:24 +0100

    Update code

--- components/DocLayout.jsx
+++ components/DocLayout.jsx
@@ -9,8 +9,8 @@ const DocLayout = ({children}) => {
 
             <Sidebar/>
 
-            <main className="transition-all duration-300 pt-[72px] lg:pl-[272px] xl:pr-[272px]">
-                <div className="transition-all duration-300 px-4 md:px-20 lg:px-28 pt-10 pb-40">
+            <main className="transition-all xl:transition-none duration-300 pt-[72px] lg:pl-[272px] xl:pr-[272px]">
+                <div className="transition-all xl:transition-none duration-300 px-4 md:px-20 lg:px-28 pt-10 pb-40">
                     {children}
                 </div>
             </main>
